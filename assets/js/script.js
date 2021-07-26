c1cbfc323c0d065856645fe572f644e1af1a8728
meinhoonharsh <harshprogrammer782@gmail.com>
2021-07-26 22:55:12 +0530

    Codebase Managed for Project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,21 @@
 // You can write JS Script here
 console.log('This Webpage is built using HTML BoilerPlate by SemiKolan')
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml6 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: (el, i) => 50 * i
+  }).add({
+    targets: '.ml6',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
